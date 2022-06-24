5f3309e589bf2cd32649fd09786f58e3f52a26c2
Michael Tejiri <105463230+Mikelscope@users.noreply.github.com>
2022-06-24 05:26:25 +0100

    Add files via upload

---
+++ myInfo.js
@@ -0,0 +1,12 @@
+//display my name
+let myName = 'Michael Okolosi Oghenetejiri';
+let nameIntro = 'My name is ';
+document.getElementById('my-info-brief').innerHTML = nameIntro + myName;
+//display my height
+let myHeight = '6ft 2inches'
+let heightIntro = 'I am approximately '
+document.getElementById('my-height').innerHTML = heightIntro + myHeight;
+//display my nationality
+let myNationality = 'Nigerian';
+let nationalityIntro = 'I am a '
+document.getElementById('my-nationality').innerHTML = nationalityIntro + myNationality;
